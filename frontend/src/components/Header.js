6a0831d0818e8e9a017786dbc928213872810bdf
iljaKorn <kornilov.ilja@rambler.ru>
2023-04-11 23:11:05 +0300

    Create main, login pages

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../images/person.png";
+
+function Header(props) {
+    return (
+    <header className="header">
+        <div className="logo">
+            <a href="/">SPORTBOX</a>
+        </div>
+        <div className="menu">
+            <ul className="left-menu">
+                <li><Link className="headLink" to="/">Главная</Link></li>
+                <li><Link className="headLink" to="/equipments">Оборудование</Link></li>
+                <li><Link className="headLink" to="/events">Мероприятия</Link></li>
+            </ul>
+            <ul className="right-menu">
+                <li>
+                    <div className="profile-icon">
+                        <a href="/"><img src={logo} height="48" width="48" alt="nice"></img></a>
+                    </div>
+                </li>
+                <li><Link className="headLink" to="/enter">Войти</Link></li>
+            </ul>
+        </div>
+    </header>)
+}
+
+export default Header
---
+++ frontend/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import * as ReactDOMClient from "react-dom/client";
+import './css/styles.css'
+import App from "./App";
+
+
+const app = ReactDOMClient.createRoot(document.getElementById("app"));
+
+app.render(
+        <App></App>
+);
+
