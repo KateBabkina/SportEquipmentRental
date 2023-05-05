6689e1f8b2b1d6b08ae52dcd7e61fcae5b489834
iljaKorn <kornilov.ilja@rambler.ru>
2023-05-05 19:39:09 +0300

    Fixed inventory page layout

--- frontend/src/components/FiltreField.js
+++ frontend/src/components/FiltreField.js
@@ -38,17 +38,17 @@ function FilterField({ changeFilter }) {
         console.log("-----------");
         console.log(filter);
         axios.post("http://localhost:8080/api/inventory_type/filter", filter,
-      {
-        auth: {
-          username: username,
-          password: password
-        }
-      }).then(res => {
-        console.log(res.data);
-        changeFilter(res.data)
-      }).catch(() => {
-        alert("An error occurred on the server")
-      })
+            {
+                auth: {
+                    username: username,
+                    password: password
+                }
+            }).then(res => {
+                console.log(res.data);
+                changeFilter(res.data)
+            }).catch(() => {
+                alert("An error occurred on the server")
+            })
     }
 
     const filtredInput = (event) => {
@@ -69,25 +69,87 @@ function FilterField({ changeFilter }) {
 
     return (
 
-        <div className="filter">
-            <h2>Фильтры поиска</h2>
-            <div className="type">
-                <h3>Тип оборудования:</h3>
-                <div>
-                    <select id="inventoryType" onChange={(e) => filtredInput(e)}>
-                        <option key="-" value="">
-                            Выберите тип
-                        </option>
-                        {getTypes()}
-                    </select>
+        <div className="left-column-filters">
+            <div className="filter-panel">
+
+                <div className="filters-label">
+                    Фильтры поиска
                 </div>
-                <h3>Дата начала аренды:</h3>
-                <input id="startDate" placeholder="гггг-мм--дд" onChange={e => filtredInput(e)}></input>
-                <h3>Дата окончания аренды:</h3>
-                <input id="endDate" placeholder="гггг-мм--дд" onChange={e => filtredInput(e)}></input>
-                <h3>Цена за день:</h3>
-                <input id="minPrice" onChange={e => filtredInput(e)}></input><input id="maxPrice" onChange={e => filtredInput(e)}></input>
-                <button onClick={sendFilter}>Найти</button>
+
+                <div className="type-equipment-box">
+
+                    <div className="label-type-equipment-box">
+                        Тип оборудования:
+                    </div>
+
+                    <div className="field-type-equipment-box">
+                        <select id="inventoryType" name="type-equipment" onChange={(e) => filtredInput(e)}>
+                            <option key="-" value="">
+                                Выберите тип
+                            </option>
+                            {getTypes()}
+                        </select>
+                    </div>
+                </div>
+
+                <div className="start-data-box">
+
+                    <div className="label-start-data-box">
+                        Дата начала аренды:
+                    </div>
+
+                    <div className="field-start-data-box">
+                        <input id="startDate" type="date" name="fullName" placeholder="гггг-мм--дд" onChange={e => filtredInput(e)}
+                            minLength="4" maxLength="35" size="20"></input>
+                    </div>
+
+                </div>
+
+                <div className="end-data-box">
+
+                    <div className="label-end-data-box">
+                        Дата окончания аренды:
+                    </div>
+
+                    <div className="field-end-data-box">
+                        <input id="endDate" type="date" name="fullName" placeholder="гггг-мм--дд" onChange={e => filtredInput(e)}
+                            minLength="4" maxLength="35" size="20"></input>
+                    </div>
+
+                </div>
+
+                <div className="price-for-day-box">
+                    <div className="label-price-for-day-box">
+                        Цена за день:
+                    </div>
+
+
+                    <div className="label-price-from">
+                        от:
+                    </div>
+                    <div className="field-price-from">
+                        <input id="minPrice" type="number" name="fullName" onChange={e => filtredInput(e)}
+                            minLength="4" maxLength="35" size="20"></input>
+                    </div>
+
+                    <div className="label-price-to">
+                        до:
+                    </div>
+
+                    <div className="field-price-to">
+                        <input id="maxPrice" type="number" name="fullName" onChange={e => filtredInput(e)}
+                            minLength="4" maxLength="35" size="20"></input>
+                    </div>
+                </div>
+
+                <div class="button-find">
+                    <button class="find-button" type="submit" onClick={sendFilter}>
+                        <div class="find-button-text">
+                            Найти
+                        </div>
+                    </button>
+                </div>
+
             </div>
         </div>
     )
--- frontend/src/components/Item.js
+++ frontend/src/components/Item.js
@@ -2,11 +2,30 @@ import React from "react";
 
 function Item({ item }) {
   return (
-    <div className="item" >
-        <img src={require("../images/inventory/" + item.type + ".jpg")} alt="MISSING JPG"/>
-        <h2>{item.type}</h2>
-        <h3>{item.price} руб./день</h3>
-        <button className="rent" >Арендовать</button>
+    <div className="equipment-wrapper">
+
+      <div className="equipment-img">
+        <img src={require("../images/inventory/" + item.type + ".jpg")} alt="MISSING JPG" />
+      </div>
+
+      <div className="equipment-info">
+        <div className="equipment-name">
+          {item.type}
+        </div>
+
+        <div className="equipment-price">
+          {item.price} руб./день
+        </div>
+      </div>
+
+      <div className="button-rent">
+        <button className="rent-button" type="submit">
+          <div className="rent-button-text">
+            Арендовать
+          </div>
+        </button>
+      </div>
+
     </div>
   );
 };
