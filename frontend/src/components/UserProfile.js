9a3db49b895f04ab1bf0d22d06d608b6a0ff65fb
iljaKorn <kornilov.ilja@rambler.ru>
2023-05-07 22:29:22 +0300

    Fixed layout profile page

--- frontend/src/components/UserProfile.js
+++ frontend/src/components/UserProfile.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
@@ -13,7 +12,7 @@ export default function EventPage({ setIsLogged }) {
   const [email, setEmail] = useState("")
 
   useEffect(() => {
-    axios.get(`http://localhost:8080/api/person/profile?id=${localStorage.getItem("userId")}`, 
+    axios.get(`http://localhost:8080/api/person/profile?id=${localStorage.getItem("userId")}`,
       {
         auth: {
           username: username,
@@ -24,7 +23,7 @@ export default function EventPage({ setIsLogged }) {
         setBooking(res.data.person.bookings)
         setName(res.data.person.name)
         setEmail(res.data.person.email)
-        
+
       }).catch(() => {
         alert("An error occurred on the server")
       })
@@ -34,28 +33,138 @@ export default function EventPage({ setIsLogged }) {
     localStorage.setItem("isLogged", false)
     setIsLogged(false)
     localStorage.setItem("userId", -1)
+    window.location.href = "/"
   }
 
   const getHistory = () => {
     return booking?.map((el) => {
-        return <div key={el.id}>
-          №{el.id} Оборудование {el.inventory.name} Цена,руб. {el.price} Дата заказа {el.date} Дата начала {el.startDate} Дата окончания {el.endDate} Долг {el.debt}
-          <button>Отменить</button>
-        </div>; //поправить чтобы выводил все данные о заказе
+      return (<div className="row" key={el.id}>
+
+        <div className="order-number-row">
+          {el.id}
+        </div>
+
+        <div className="order-equipment-row">
+          {el.inventory.name}
+        </div>
+
+        <div className="order-price-row">
+          {el.price}
+        </div>
+
+        <div className="order-data-row">
+          {el.date}
+        </div>
+
+        <div className="order-data-from-row">
+          {el.startDate}
+        </div>
+
+        <div className="order-data-to-row">
+          {el.endDate}
+        </div>
+
+        <div className="order-debt-row">
+          {el.debt}
+        </div>
+
+        <div className="order-action-row">
+          <div className="button-cancel">
+            <button className="cancel-button" type="submit">
+              <div className="cancel-button-text">
+                Отменить
+              </div>
+            </button>
+          </div>
+        </div>
+
+      </div>)
     });
-}
+  }
 
   return (
-    <div>
-      <h1>Информация о пользователе</h1>
-      <div>
-        <h1>{name}</h1>
-        <h2>{email}</h2>
+    <div className="profile-wrapper">
+
+      <div className="information-about-user-lable">
+        <p>Информация о пользователе:</p>
       </div>
-      <h1>История заказов</h1>
-      {booking?.length === 0 ? <h3>Empty</h3>: getHistory()}
-      
-      <h1><Link className="headLink" to="/" onClick={logOut}>Выйти</Link></h1>
+
+      <div className="information-about-user-wrapper">
+
+        <div className="full-name-user-lable">
+          ФИО:
+        </div>
+        <div className="full-name-user">
+          {name}
+        </div>
+
+        <div className="email-user-lable">
+          Алрес эл. почты:
+        </div>
+
+        <div className="email-user">
+          {email}
+        </div>
+
+      </div>
+
+      <div className="information-about-orders-wrapper">
+
+        <div className="information-about-orders-lable">
+          История заказов:
+        </div>
+
+        <div className="column-lables">
+
+          <div className="order-number-lable">
+            №
+          </div>
+
+          <div className="order-equipment-lable">
+            Оборудование
+          </div>
+
+          <div className="order-price-lable">
+            Цена, руб.
+          </div>
+
+          <div className="order-data-lable">
+            Дата заказа
+          </div>
+
+          <div className="order-data-from-lable">
+            Дата начала
+          </div>
+
+          <div className="order-data-to-lable">
+            Дата окончания
+          </div>
+
+          <div className="order-debt-lable">
+            Долг, руб.
+          </div>
+
+          <div className="order-action-lable">
+
+          </div>
+        </div>
+
+        <div className="information-about-orders-table-wrapper">
+          <div className="table-rows">
+            {booking?.length === 0 ? <h3>Пусто</h3> : getHistory()}
+          </div>
+        </div>
+
+      </div>
+
+      <div className="button-exit">
+            <button className="exit-button" type="submit" onClick={logOut}>
+                <div className="exit-button-text">
+                    Выйти
+                </div>
+            </button>
+        </div>
+
     </div>
   );
 };
