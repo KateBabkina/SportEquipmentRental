d76311cbd933e4b85946fbf83197921265bfde2b
SolidSnakeF141 <ignat_kandaurov@mail.ru>
2023-05-21 12:42:12 +0300

    Updated SplashScreen logo

--- frontend/src/components/SplashScreen.js
+++ frontend/src/components/SplashScreen.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import ClipLoader from "react-spinners/ClipLoader";
+
 import classes from '../css/main_page.module.css';
 
 export default function SplashScreen({ loading }) {
     return (
         <div>
-            <div className={classes.logo}>
+            <div className={classes.logoClip}>
                 SPORTBOX
             </div>
             <h1 style={{ textAlign: 'center' }}>Приложение загружается. Пожалуйста подождите.</h1>
